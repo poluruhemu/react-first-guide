be9950c4ffabf067551a28708387dbca3a69a330
poluruhemu <poluruhemu@gmail.com>
2022-05-11 07:26:45 +0530

    React composition

---
+++ src/Errorhandler/ErrorHandler.js
@@ -0,0 +1,49 @@
+import * as React from 'react';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+function CustomizedSnackbars(props,ref) {
+  const [open, setOpen] = React.useState(false);
+  const [message,setMessage] = React.useState('none');
+
+
+  React.useImperativeHandle( ref, () => ({
+      foo(msg){
+          setMessage(msg);
+          handleClick();
+      }
+  }),[]);
+
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  return (
+    <Stack spacing={2} sx={{ width: '100%' }}>
+      <Button variant="outlined" onClick={handleClick}>
+        Open success snackbar
+      </Button>
+      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="error" sx={{ width: '100%' }}>
+          {message}
+        </Alert>
+      </Snackbar>
+    </Stack>
+  );
+}
+
+export default React.forwardRef(CustomizedSnackbars)
